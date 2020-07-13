22b2bd30da93a650e2a162963b65aa46b4cbcfca
Inoxevious <mpasiinnocent@gmail.com>
2020-07-13 03:13:11 +0200

    update

---
+++ backoffice/management/commands/makesuper.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+class Command(BaseCommand):
+  def handle(self, *args, **options):
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+      User.objects.create_superuser("admin", "admin@gmail.com", "Malin@1#")
+      self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
